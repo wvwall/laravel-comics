5e008d7fdb489ffa733f91d5c327da8ff1f8a389
wvwall <waltervelardo@gmail.com>
2021-05-12 17:48:12 +0200

    Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,12 +46,84 @@
                     </div>
                 </nav>
             </section>
+            <section class="jumbo">
+                <div class="container-jumbo"></div>
+            </section>
         </header>
         <main>
              @yield('main')
         </main>
         <footer>
-
+            <section>
+                <div class="footer-top">
+                    <div class="container">
+                        <div class="left-container">
+                            <div class="ul-left">
+                                <div>
+                                <ul>
+                                    <li>dc comics</li>
+                                    <li><a href="">Characters</a></li>
+                                    <li><a href="">Comics</a></li>
+                                    <li><a href="">Movies</a></li>
+                                    <li><a href="">TV</a></li>
+                                    <li><a href="">Games</a></li>
+                                    <li><a href="">Videos</a></li>
+                                    <li><a href="">News</a></li>
+                                </ul>
+                                <ul>
+                                    <li>shop</li>
+                                    <li><a href="">Shop DC</a></li>
+                                    <li><a href="">Shop DC Collectibles</a></li>
+                                </ul>
+                                </div>
+                               
+                                <ul>
+                                    <li>dc </li>
+                                    <li><a href="">Terms Of Use</a></li>
+                                    <li><a href="">Privacy policy(New)</a></li>
+                                    <li><a href="">Ad Choices</a></li>
+                                    <li><a href="">Advertising</a></li>
+                                    <li><a href="">Jobs</a></li>
+                                    <li><a href="">Subscriptions</a></li>
+                                    <li><a href="">Talent Workshops</a></li>
+                                    <li><a href="">CPSC Certificates</a></li>
+                                    <li><a href="">Rating</a></li>
+                                    <li><a href="">Shop Help</a></li>
+                                    <li><a href="">Contact Us</a></li>
+                                </ul>
+                                <ul>
+                                    <li>sites </li>
+                                    <li><a href="">DC</a></li>
+                                    <li><a href="">MAD Magazine</a></li>
+                                    <li><a href="">DC Kids</a></li>
+                                    <li><a href="">DC Universe</a></li>
+                                    <li><a href="">DC Power Visa</a></li>
+                                </ul>
+                            </div>
+                            <p>All site Content TM 2020 DC Entertainment, unless otherwise <span>noted here</span>. All rights reserved. <span>Cookies Settings</span> </p>
+                        </div>
+                        <div class="right-container">
+                            <img src="./images/dc-logo-bg-Copia.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-bottom">
+                    <div class="container">
+                        <a href="">Sign-up now!</a>
+                        <div class="right-footer">
+                        <span>follow us</span>
+                        <div class="icons">
+                            <img src="./images/footer-facebook.png" alt="">
+                            <img src="./images/footer-twitter.png" alt="">
+                            <img src="./images/footer-youtube.png" alt="">
+                            <img src="./images/footer-pinterest.png" alt="">
+                            <img src="./images/footer-periscope.png" alt="">
+                        </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
         </footer>   
 
 
